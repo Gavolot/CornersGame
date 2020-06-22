8e255a79786e2a6c70436d61b5e602016a37c405
Gavolot <assertiven@yandex.ru>
2020-06-23 01:57:52 +0300

    small fix

--- Corners/Assets/Scripts/Board.cs
+++ Corners/Assets/Scripts/Board.cs
@@ -9,6 +9,7 @@ public class Board : MonoBehaviour {
     public int horizontalCells = 8;
     public int verticalCells = 8;
 
+    private int board_length = 0;
 
     private SpriteRenderer[] cellSprites;
     private GameObject[] gameObjects;
@@ -18,32 +19,27 @@ public class Board : MonoBehaviour {
         var sizeV = blackCellSprite.bounds.size.y;
         Vector2 pos = Vector2.zero;
         GameObject lastObject = null;
-        int len = horizontalCells*verticalCells;
-        Debug.Log(len);
+        board_length  = horizontalCells * verticalCells;
         var next = 0;
-        gameObjects = new GameObject[len];
-        cellSprites = new SpriteRenderer[len];
-
+        gameObjects = new GameObject[board_length];
+        cellSprites = new SpriteRenderer[board_length ];
+        bool change = false;
+        var backgroundColor = blackCellSprite;
+        //var l = 0;
 
         for (int i = 0; i < verticalCells; i++) {
             for (int j = 0; j < horizontalCells; j++) {
                 pos = new Vector2 (sizeH * j, sizeV * i);
                 lastObject = Instantiate (prefab, pos, Quaternion.identity, transform);
                 gameObjects[next] = lastObject;
-                cellSprites[next] = lastObject.GetComponent<SpriteRenderer>();
+                cellSprites[next] = lastObject.GetComponent<SpriteRenderer> ();
+
+                change = next % 8 == 0 || next == 0;
+                backgroundColor = change ? backgroundColor : backgroundColor == whiteCellSprite ? blackCellSprite : whiteCellSprite;
+                lastObject.GetComponent<SpriteRenderer> ().sprite = backgroundColor;
+                change = false;
                 next++;
             }
-        }
-
-        bool change = false;
-
-        var backgroundColor = blackCellSprite;
-        
-        for(int i = 0; i<len; ++i){
-            change = i % 8 == 0 || i == 0;
-            backgroundColor = change ? backgroundColor : backgroundColor == whiteCellSprite ? blackCellSprite : whiteCellSprite;
-            cellSprites[i].sprite = backgroundColor;
-            change = false;
         }
 
         #region centeredToBoard
@@ -55,5 +51,16 @@ public class Board : MonoBehaviour {
     }
     void Update () {
 
+    }
+
+    private void ColoredChessBoard () {
+        bool change = false;
+        var backgroundColor = blackCellSprite;
+        for (int i = 0; i < board_length; ++i) {
+            change = i % 8 == 0 || i == 0;
+            backgroundColor = change ? backgroundColor : backgroundColor == whiteCellSprite ? blackCellSprite : whiteCellSprite;
+            cellSprites[i].sprite = backgroundColor;
+            change = false;
+        }
     }
 }
